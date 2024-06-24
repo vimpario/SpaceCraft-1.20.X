8e65214cb3090e311028a2b925964483d795ccb3
vimpario <koz.viktor2020@gmail.com>
2024-06-24 18:52:09 +0300

    Deleted comments in SpaceCraft class

--- src/main/java/net/vimpario/spacecraft/SpaceCraft.java
+++ src/main/java/net/vimpario/spacecraft/SpaceCraft.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(SpaceCraft.MOD_ID)
 public class SpaceCraft
 {
@@ -31,7 +31,7 @@ public class SpaceCraft
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
-
+    //ksdjfhjhjdshf
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event)
@@ -39,14 +39,14 @@ public class SpaceCraft
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
