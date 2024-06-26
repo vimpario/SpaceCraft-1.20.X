ad089fcc60d8c7f330bbe6f84caa433e23e08c97
vimpario <koz.viktor2020@gmail.com>
2024-06-26 12:43:39 +0300

    add aluminium (as example for other) items: -no name -no texture -no creative tab

--- src/main/java/net/vimpario/spacecraft/item/ModItems.java
+++ src/main/java/net/vimpario/spacecraft/item/ModItems.java
@@ -4,11 +4,14 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.vimpario.spacecraft.SpaceCraft;
 
 public class ModItems {
     public  static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SpaceCraft.MOD_ID);
 
+    public static final RegistryObject<Item> ALUMINIUM = ITEMS.register("aluminium",
+            () -> new Item(new Item.Properties()));
 
 
     public static void register(IEventBus eventBus) {
